81030b34670ad4279868522ed0edd3e86373e6b7
AlexysVera12 <valexys764@gmail.com>
2025-06-09 07:43:29 -0500

    Clases 1 se creo la clase Auto y la clase TestAuto

---
+++ clases/src/com/krakedev/TestAuto.java
@@ -0,0 +1,24 @@
+package com.krakedev;
+
+public class TestAuto {
+
+	public static void main(String[] args) {
+		Auto auto1 = new Auto();
+		Auto auto2 = new Auto();
+		auto1.marca = "Dodge";
+		auto1.anio = 2010;
+		auto1.precio = 20000.00;
+		auto2.marca = "Mitsubishi";
+		auto2.anio = 2015;
+		auto2.precio = 15000.00;
+		System.out.println("----------Auto 1----------");
+		System.out.println("Marca: "+auto1.marca);
+		System.out.println("Año: "+auto1.anio);
+		System.out.println("precio: "+auto1.precio);
+		System.out.println("----------Auto 2----------");
+		System.out.println("Marca: "+auto2.marca);
+		System.out.println("Año: "+auto2.anio);
+		System.out.println("Precio: "+auto2.precio);
+	}
+
+}
