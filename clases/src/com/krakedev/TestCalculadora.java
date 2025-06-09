b53bce041f109a25474d96d054906ffed32a7e7f
AlexysVera12 <valexys764@gmail.com>
2025-06-09 10:43:25 -0500

    Clases-Metodos 3 se creo el metodo promediar y se la invoco ela clase TestCalculadora

--- clases/src/com/krakedev/TestCalculadora.java
+++ clases/src/com/krakedev/TestCalculadora.java
@@ -8,15 +8,18 @@ public class TestCalculadora {
 		int resultadoResta;
 		double productoMultiplicacion;
 		double cocienteDivision;
+		double resultadoPromedio;
 		calcu = new Calculadora();
 		resultadoSuma = calcu.sumar(5, 8);
 		resultadoResta = calcu.restar(8, 5);
 		productoMultiplicacion = calcu.multiplicar(10, 5);
 		cocienteDivision = calcu.dividir(10, 2);
+		resultadoPromedio = calcu.promediar(10, 8, 9);
 		System.out.println("Resultado suma: "+resultadoSuma);
 		System.out.println("Resultado resta: "+resultadoResta);
 		System.out.println("Resultado mutiplicacion: "+productoMultiplicacion);
 		System.out.println("Resultado division: "+cocienteDivision);
+		System.out.println("Resultado promedio: "+resultadoPromedio);
 	}
 
 }
