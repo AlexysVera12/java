8151a54e98c7a7cc89843ac6a22be05c49f6c641
AlexysVera12 <valexys764@gmail.com>
2025-06-30 09:31:36 -0500

    Actividad maven

---
+++ Herencia/src/com/krakedev/herencia/TestPersona.java
@@ -0,0 +1,11 @@
+package com.krakedev.herencia;
+
+public class TestPersona {
+
+	public static void main(String[] args) {
+		Persona p = new Persona("1206984815","Alexys");
+		
+		System.out.println(p);
+	}
+
+}
